a46bcbb5ce20b41a8094da37d1e153f3c4bd6b0c
Ted Duncan <tdunca7@gmail.com>
2025-07-24 10:45:01 -0400

    add health check

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from routers import routers_v1
+from routers import routers_health
 
 def create_app():
     app = FastAPI()
+    # v1 api
     app.include_router(routers_v1.router, prefix="/v1")
+    # health check
+    app.include_router(routers_health.router)
     return app
 app = create_app()
 
